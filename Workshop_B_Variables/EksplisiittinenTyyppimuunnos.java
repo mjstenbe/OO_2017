65f2c582d97b953e434874eecce7817558246a20
antdear <antonius.camara@gmail.com>
2017-02-01 14:16:58 +0200

    Small fixes

--- Workshop_B_Variables/EksplisiittinenTyyppimuunnos.java
+++ Workshop_B_Variables/EksplisiittinenTyyppimuunnos.java
@@ -3,7 +3,9 @@ package Workshop_B_Variables;
 public class EksplisiittinenTyyppimuunnos {
 
 	public static void main(String[] args) {
-		System.out.println((int)4.7654);
+		
+		double decimalNr = 4.7654;
+		System.out.println((int)decimalNr);
 	}
 
 }
