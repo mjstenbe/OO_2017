b2c2e26c275d10e30a86564a48aa8b5eb41b1152
antdear <antonius.camara@gmail.com>
2017-03-22 15:44:54 +0200

    Improvements

--- Workshop_E_Methods/LukujenSumma.java
+++ Workshop_E_Methods/LukujenSumma.java
@@ -5,16 +5,16 @@ public class LukujenSumma {
 	public static void main(String[] args) {
 
 		// Suoritetaan aliohjelma, ei oteta palautusarvoa talteen
-		LukujenSumma(11, 8);
-		LukujenSumma(8, 4);
+		summa(11, 8);
+		summa(8, 4);
 
 		// Suoritetaan aliohjelma, otetaan palautusarvo talteen
-		int tulos = LukujenSumma(5, 2);
+		int tulos = summa(5, 2);
 		System.out.println(tulos);
 
 	}
 
-	public static int LukujenSumma(int ekaluku, int tokaluku) {
+	public static int summa(int ekaluku, int tokaluku) {
 		int summa = ekaluku + tokaluku;
 		System.out.println("Summataan: " + summa);
 		return summa;
